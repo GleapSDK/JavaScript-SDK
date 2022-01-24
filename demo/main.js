14f2ca203fd6fc1c6191b06bbe06c7b9b612a705
Lukas Boehler <lukas@boehlerbrothers.com>
2022-01-24 16:53:31 +0100

    Cleanup.

--- demo/main.js
+++ demo/main.js
@@ -1,6 +1,6 @@
 const Gleap = window.Gleap;
 
-// Gleap.setApiUrl("http://0.0.0.0:9000");
+Gleap.setApiUrl("http://localhost:9000");
 Gleap.initialize("TRiC1FmOEt2N6KCUiLIetFHyyGyxfXYV");
 Gleap.identify("613b5dc530aed737108f87a8", {
   email: "lukas@gleap.io",
