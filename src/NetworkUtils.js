d97830d948745e3332af1182615acaf527412688
Lukas Boehler <lukas@boehlerbrothers.com>
2022-02-10 11:34:44 +0100

    v6.7.9

--- src/NetworkUtils.js
+++ src/NetworkUtils.js
@@ -3,6 +3,7 @@ export const isLocalNetwork = function (hostname = window.location.hostname) {
     ["localhost", "127.0.0.1", "0.0.0.0", "", "::1"].includes(hostname) ||
     hostname.startsWith("192.168.") ||
     hostname.startsWith("10.0.") ||
-    hostname.endsWith(".local")
+    hostname.endsWith(".local") ||
+    !hostname.includes(".")
   );
 };
