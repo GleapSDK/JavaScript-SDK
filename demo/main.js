4e850c8b6f3f825eadc8051e6ae4d42091c5e4a2
Lukas Boehler <lukas@boehlerbrothers.com>
2022-05-31 16:30:56 +0200

    Update main.js

--- demo/main.js
+++ demo/main.js
@@ -1,6 +1,6 @@
 const Gleap = window.Gleap;
 
-Gleap.setApiUrl("http://localhost:9000");
+// Gleap.setApiUrl("http://localhost:9000");
 Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
 
 Gleap.attachCustomData({
