d680fcb2c147efe1d735a41a7f75e9b5e5574f6d
Lukas Boehler <lukas@boehlerbrothers.com>
2022-07-21 16:12:28 +0200

    Update GleapAudioManager.js

--- src/GleapAudioManager.js
+++ src/GleapAudioManager.js
@@ -4,7 +4,7 @@ export default class GleapAudioManager {
     static ping() {
         try {
             if (!this.audio) {
-                this.audio = new Audio('https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3');
+                this.audio = new Audio('https://js.gleap.io/res/ping.mp3');
             }
 
             this.audio.play();
