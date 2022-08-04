e010074bbfdb005aa5f806b91b817686f0361625
Lukas Boehler <lukas@boehlerbrothers.com>
2022-08-04 17:08:30 +0200

    Cleanup.

--- demo/main.js
+++ demo/main.js
@@ -1,9 +1,9 @@
 const Gleap = window.Gleap;
 
-// Gleap.setFrameUrl("http://localhost:3001");
+Gleap.setFrameUrl("http://localhost:3001");
 // Gleap.setLanguage("en");
-// Gleap.setApiUrl("http://localhost:9000");
-Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
+Gleap.setApiUrl("http://localhost:9000");
+Gleap.initialize("KProDXhMS0V3UUku2iNnrZ4XsBnAYzxt");
 
 Gleap.identify("user_19283", {
   name: "Franz",
