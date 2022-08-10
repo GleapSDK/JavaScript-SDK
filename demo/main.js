21b6ae9ee8104c81d8685c937ebac1083f1215de
Lukas Boehler <lukas@boehlerbrothers.com>
2022-08-10 13:45:23 +0200

    Update main.js

--- demo/main.js
+++ demo/main.js
@@ -12,39 +12,19 @@ Gleap.identify("user_19283", {
   value: 199
 });
 
-Gleap.attachCustomData({
-  mission: "Unicorn",
-  type: "Demo App",
-  nestedData: {
-    possible: true,
-    name: "Mission: Impossible - Ghost Protocol",
-  },
-});
-
 Gleap.log("Test log");
 Gleap.log("Test log info", "INFO");
 Gleap.log("Test log warn", "WARNING");
 Gleap.log("Test log err", "ERROR");
 
-// Register custom action.
-Gleap.registerCustomAction((customAction) => {
-  console.log("Custom action called:");
-  console.log(customAction);
+Gleap.on("open", () => {
+  // Attach custom data.
+  Gleap.attachCustomData({
+    mission: "Unicorn",
+    type: "Demo App",
+    nestedData: {
+      possible: true,
+      name: "Mission: Impossible - Ghost Protocol",
+    },
+  });
 });
-
-// Network logs test.
-setTimeout(() => {
-  console.log("Loading place infos");
-
-  var xmlhttp = new XMLHttpRequest();
-  var url = "https://jsonplaceholder.typicode.com/todos/1";
-
-  xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var myArr = JSON.parse(this.responseText);
-      console.log(myArr);
-    }
-  };
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
-}, 3000);
