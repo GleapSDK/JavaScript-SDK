8c0669f0e7d33b0a664c6e9310604d23f8db3992
Lukas Boehler <lukas@boehlerbrothers.com>
2023-03-13 19:39:33 +0100

    Bot.

--- demo/main.js
+++ demo/main.js
@@ -1,7 +1,7 @@
 const Gleap = window.Gleap;
 
-//Gleap.setFrameUrl("http://0.0.0.0:3001");
-Gleap.setApiUrl("https://api.gleap.io");
+Gleap.setFrameUrl("http://0.0.0.0:3001");
+Gleap.setApiUrl("http://0.0.0.0:9000");
 Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
 //Gleap.setEnvironment("dev");
 
