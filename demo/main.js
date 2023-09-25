43963dccf08c33f55af7fe57d5bfb40c7627c935
Lukas Böhler <lukas@boehlerbrothers.com>
2023-09-25 10:25:17 +0200

    Fix.

--- demo/main.js
+++ demo/main.js
@@ -1,13 +1,13 @@
 const Gleap = window.Gleap;
 
-Gleap.setFrameUrl("http://0.0.0.0:3001");
-Gleap.setApiUrl("http://0.0.0.0:9000");
+// Gleap.setFrameUrl("http://0.0.0.0:3001");
+// Gleap.setApiUrl("http://0.0.0.0:9000");
 
 Gleap.setDisablePageTracking(true);
 
 Gleap.setLanguage("en");
 
-Gleap.initialize("hciNpT8z64tsHATINYZjWBvbirVWCKWt");
+Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
 
 /*Gleap.setUrlHandler((url, newTab) => {
   alert("URL: " + url + " newTab: " + newTab);
