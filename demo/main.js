b4a5cca58c5f38ca80c909bae9a96dd3539d165c
Lukas Böhler <lukas@boehlerbrothers.com>
2023-11-20 13:36:40 +0100

    Fixed npm start.

--- demo/main.js
+++ demo/main.js
@@ -6,7 +6,7 @@ Gleap.setWSApiUrl("ws://0.0.0.0:8080");
 
 // Gleap.setLanguage("en");
 
-Gleap.initialize("kqHBGdyxG0qq6gcfHDqgNkxg5r0Ut0Le");
+Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
 
 /*Gleap.setUrlHandler((url, newTab) => {
   alert("URL: " + url + " newTab: " + newTab);
