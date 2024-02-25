b92c5dc232fbb568e76e4685d4c575b52a47d716
Lukas Böhler <lukas@boehlerbrothers.com>
2024-02-25 21:02:22 +0100

    v12.3.7
    Added support for ESM as well as CommonJS.

--- demo/main.js
+++ demo/main.js
@@ -1,10 +1,10 @@
 const Gleap = window.Gleap;
 
-Gleap.setFrameUrl("http://0.0.0.0:3001");
+/*Gleap.setFrameUrl("http://0.0.0.0:3001");
 Gleap.setApiUrl("http://0.0.0.0:9000");
-Gleap.setWSApiUrl("ws://0.0.0.0:8080");
+Gleap.setWSApiUrl("ws://0.0.0.0:8080");*/
 
-Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
+Gleap.initialize("44iWQrD3YuDxqj6aw5HqsKyuaas28ZKu");
 
 /*Gleap.setUrlHandler((url, newTab) => {
   alert("URL: " + url + " newTab: " + newTab);
--- webpack.config.js
+++ webpack.config.js
@@ -3,81 +3,135 @@ const webpack = require("webpack");
 const exec = require("child_process").exec;
 const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  mode: "production",
-  entry: {
-    index: "./src/index.js",
+const copyBuildPlugin = {
+  apply: (compiler) => {
+    compiler.hooks.afterEmit.tap("AfterEmitPlugin", (compilation) => {
+      const nodeVersion = process.env.npm_package_version;
+      return exec(
+        `mkdir -p published/${nodeVersion} & mkdir -p published/latest & cp ./build/cjs/index.js published/${nodeVersion}/index.js & cp ./build/cjs/index.js published/latest/index.js`,
+        (err, stdout, stderr) => {
+          if (stdout) process.stdout.write(stdout);
+          if (stderr) process.stderr.write(stderr);
+          console.log("DONE 🎉");
+        }
+      );
+    });
   },
+};
+
+// Common configuration
+const commonConfig = (isDevelopment, plugins = []) => {
+  var config = {
+    mode: "production",
+    entry: {
+      index: "./src/index.js",
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            mangle: true,
+            sourceMap: true,
+            safari10: true,
+            output: {
+              comments: false,
+            },
+          },
+          extractComments: false,
+        }),
+      ],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+          use: ["url-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        SDK_VERSION: JSON.stringify(process.env.npm_package_version),
+      }),
+      ...plugins,
+    ],
+  };
+
+  if (isDevelopment) {
+    config.mode = "development";
+    config.devServer = {
+      compress: true,
+      open: true,
+      hot: true,
+      host: '0.0.0.0',
+      static: [
+        {
+          directory: path.join(__dirname, 'demo'),
+        },
+        {
+          directory: path.join(__dirname, 'build'),
+          publicPath: '/build',
+        },
+      ],
+      port: 4444,
+    };
+  }
+
+  return config;
+};
+
+// Configuration for ESM
+const esmConfig = {
+  ...commonConfig(false, []),
+  output: {
+    filename: "[name].mjs",
+    path: path.resolve(__dirname, "build/esm"),
+    library: {
+      type: "module",
+    },
+    globalObject: "this",
+    clean: true,
+  },
+  experiments: {
+    outputModule: true,
+  },
+};
+
+// Configuration for CJS
+const cjsConfig = {
+  ...commonConfig(false, [copyBuildPlugin]),
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "build"),
-    library: "Gleap",
+    path: path.resolve(__dirname, "build/cjs"),
     libraryTarget: "umd",
+    library: "Gleap",
     libraryExport: "default",
     globalObject: "this",
     clean: true,
   },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          mangle: true,
-          sourceMap: true,
-          safari10: true,
-          output: {
-            comments: false,
-          },
-        },
-        extractComments: false,
-      }),
-    ],
-  },
-  devServer: {
-    open: true,
-    hot: true,
-    host: "0.0.0.0",
-    static: path.join(__dirname, "demo"),
-    port: 4444,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
-        use: ["url-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      SDK_VERSION: JSON.stringify(process.env.npm_package_version),
-    }),
-    {
-      apply: (compiler) => {
-        compiler.hooks.afterEmit.tap("AfterEmitPlugin", (compilation) => {
-          const nodeVersion = process.env.npm_package_version;
-          return exec(
-            `mkdir -p published/${nodeVersion} & mkdir -p published/latest & cp ./build/index.js published/${nodeVersion}/index.js & cp ./build/index.js published/latest/index.js`,
-            (err, stdout, stderr) => {
-              if (stdout) process.stdout.write(stdout);
-              if (stderr) process.stderr.write(stderr);
+};
 
-              console.log("DONE 🎉");
-            }
-          );
-        });
-      },
-    },
-  ],
+const developmentConfig = {
+  ...commonConfig(true, []),
+  // ... additional development-specific settings ...
+};
+
+module.exports = (env) => {
+  if (env && env.development) {
+    return developmentConfig;
+  } else {
+    return [esmConfig, cjsConfig];
+  }
 };
