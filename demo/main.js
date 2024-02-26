d37b18e18b2c6c7fa8c812e3f65d898c83165b7e
Lukas Böhler <lukas@boehlerbrothers.com>
2024-02-26 21:20:24 +0100

    v13.2.8

--- demo/main.js
+++ demo/main.js
@@ -1,10 +1,10 @@
 const Gleap = window.Gleap;
 
-/*Gleap.setFrameUrl("http://0.0.0.0:3001");
+Gleap.setFrameUrl("http://0.0.0.0:3001");
 Gleap.setApiUrl("http://0.0.0.0:9000");
-Gleap.setWSApiUrl("ws://0.0.0.0:8080");*/
+Gleap.setWSApiUrl("ws://0.0.0.0:8080");
 
-Gleap.initialize("44iWQrD3YuDxqj6aw5HqsKyuaas28ZKu");
+Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
 
 /*Gleap.setUrlHandler((url, newTab) => {
   alert("URL: " + url + " newTab: " + newTab);
