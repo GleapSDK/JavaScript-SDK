18486e76a1ea1c61f22025e806df8e16374ad9af
Lukas Boehler <lukas@gleap.io>
2024-09-14 08:53:02 +0200

    Update main.js

--- demo/main.js
+++ demo/main.js
@@ -4,4 +4,4 @@ Gleap.setFrameUrl("http://0.0.0.0:3001");
 Gleap.setApiUrl("http://0.0.0.0:9000");
 Gleap.setWSApiUrl("ws://0.0.0.0:9000");
 
-Gleap.initialize("44iWQrD3YuDxqj6aw5HqsKyuaas28ZKu");
+Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
