e6e7635a6e7d985be5de5cacd4619e3064c263a0
Lukas Boehler <lukas@gleap.io>
2024-12-20 07:51:56 +0100

    v14.1.1

--- src/GleapProductTours.js
+++ src/GleapProductTours.js
@@ -2,244 +2,260 @@ import { loadIcon } from "./UI";
 import GleapTours from "./GleapTours";
 
 export default class GleapProductTours {
-    productTourData = undefined;
-    productTourId = undefined;
-    onCompletion = undefined;
-    unmuted = false;
+  productTourData = undefined;
+  productTourId = undefined;
+  onCompletion = undefined;
+  unmuted = false;
 
-    // GleapReplayRecorder singleton
-    static instance;
-    static getInstance() {
-        if (!this.instance) {
-            this.instance = new GleapProductTours();
-            return this.instance;
-        } else {
-            return this.instance;
-        }
+  // GleapReplayRecorder singleton
+  static instance;
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new GleapProductTours();
+      return this.instance;
+    } else {
+      return this.instance;
+    }
+  }
+
+  constructor() {}
+
+  startWithConfig(tourId, config, onCompletion) {
+    this.productTourId = tourId;
+    this.productTourData = config;
+    this.onCompletion = onCompletion;
+
+    return this.start();
+  }
+
+  start() {
+    const config = this.productTourData;
+    if (!config) {
+      return;
     }
 
-    constructor() { }
+    this.unmuted = false;
+    const steps = config.steps;
+    const self = this;
 
-    startWithConfig(tourId, config, onCompletion) {
-        this.productTourId = tourId;
-        this.productTourData = config;
-        this.onCompletion = onCompletion;
+    var driverSteps = [];
 
-        return this.start();
-    }
+    for (let i = 0; i < steps.length; i++) {
+      const step = steps[i];
 
-    start() {
-        const config = this.productTourData;
-        if (!config) {
-            return;
-        }
+      const isClickMode = step.mode === "CLICK";
+      const isInputMode = step.mode === "INPUT";
 
-        this.unmuted = false;
-        const steps = config.steps;
-        const self = this;
+      var message = "";
+      var hasSender = false;
 
-        var driverSteps = [];
-
-        for (let i = 0; i < steps.length; i++) {
-            const step = steps[i];
-
-            const isClickMode = step.mode === "CLICK";
-            const isInputMode = step.mode === "INPUT";
-
-            var message = "";
-            var hasSender = false;
-
-            if (step.type === "video-pointer") {
-                message = `<div class="gleap-tour-video">
+      if (step.type === "video-pointer") {
+        message = `<div class="gleap-tour-video">
               <video class="gleap-tour-video-obj" muted autoplay>
                 <source src="${step.videoUrl}" type="video/mp4">
               </video>
-              <div class="gleap-tour-video-playpause">${loadIcon("unmute")}</div>
+              <div class="gleap-tour-video-playpause">${loadIcon(
+                "unmute"
+              )}</div>
             </div>`;
-            } else {
-                var senderHTML = ``;
+      } else {
+        var senderHTML = ``;
 
-                if (config.sender && config.sender.firstName) {
-                    hasSender = true;
-                    senderHTML = `<div class="gleap-tour-sender">
+        if (config.sender && config.sender.firstName) {
+          hasSender = true;
+          senderHTML = `<div class="gleap-tour-sender">
                 <div class="gleap-tour-sender-image" style="background-image: url('${config.sender.profileImageUrl}');"></div>
                 <div class="gleap-tour-sender-name">${config.sender.firstName}</div>
               </div>`;
-                }
-
-                message = `${senderHTML}<div class="gleap-tour-message">${step.message}</div>`;
-            }
-
-            const disableInteraction = !isClickMode && !isInputMode;
-
-            var driverStep = {
-                disableActiveInteraction: disableInteraction,
-                mode: step.mode,
-                popover: {
-                    description: message,
-                    popoverClass: `gleap-tour-popover-${step.type} ${!hasSender && 'gleap-tour-popover-no-sender'} ${config.allowClose && 'gleap-tour-popover-can-close'}`,
-                    ...(isClickMode ? {
-                        showButtons: [],
-                    } : {})
-                },
-            }
-            if (step.selector && step.selector.length > 0) {
-                driverStep.element = step.selector;
-            }
-            driverSteps.push(driverStep);
         }
 
-        var buttons = [
-            'next',
-            'close'
-        ];
+        message = `${senderHTML}<div class="gleap-tour-message">${step.message}</div>`;
+      }
 
-        if (config.backButton) {
-            buttons.push('previous');
-        }
+      const disableInteraction = !isClickMode && !isInputMode;
 
-        function onDocumentClick(evnt) {
-            var gleapTourPopover = document.querySelector('.gleap-tour-popover');
-            if (!gleapTourPopover.contains(evnt.target)) {
-                const stepIndex = gleapTourObj.getActiveIndex();
-                const step = steps[stepIndex];
-                const element = gleapTourObj.getActiveElement();
-
-                if (step?.mode === "CLICK" && evnt?.target !== element) {
-                    const isInsideElement = element.contains(evnt?.target);
-
-                    if (!isInsideElement) {
-                        // Ignore clicks outside of the actual element.
-                        return;
-                    }
-                }
-
-                if ((element && element.tagName === 'INPUT') || step.mode === "INPUT" || evnt?.target?.id.includes("tooltip-svg")) {
-                    // Prevent.
-                } else {
-                    gleapTourObj.moveNext();
-                }
-            }
-        }
-
-        const gleapTourObj = GleapTours({
-            showProgress: true,
-            steps: driverSteps,
-            showProgress: steps.length > 1,
-            allowClose: config.allowClose,
-            nextBtnText: config.nextText,
-            doneBtnText: config.doneText,
-            prevBtnText: config.prevText,
-            showButtons: buttons,
-            onDestroyStarted: () => {
-                if (!gleapTourObj.hasNextStep()) {
-                    gleapTourObj.destroy();
-
-                    if (self.onCompletion) {
-                        self.onCompletion({
-                            tourId: self.productTourId
-                        });
-                    }
-                } else {
-                    gleapTourObj.destroy();
-                }
-
-                document.removeEventListener("click", onDocumentClick);
-            },
-            onPopoverRender: (popoverElement) => {
-                // Fix for images and videos.
-                if (popoverElement) {
-                    const mediaElements = document.querySelectorAll('.gleap-tour-popover-description img, .gleap-tour-popover-description video');
-
-                    const performRequentialRefresh = () => {
-                        setTimeout(() => {
-                            gleapTourObj.refresh();
-                        }, 500);
-                        setTimeout(() => {
-                            gleapTourObj.refresh();
-                        }, 750);
-                    };
-
-                    for (let i = 0; i < mediaElements.length; i++) {
-                        const mediaElement = mediaElements[i];
-                        if (mediaElement.tagName === 'IMG') {
-                            mediaElement.addEventListener('load', () => {
-                                performRequentialRefresh();
-                            });
-                            mediaElement.addEventListener('error', () => {
-                                performRequentialRefresh();
-                            });
-                        } else if (mediaElement.tagName === 'VIDEO') {
-                            mediaElement.addEventListener('canplaythrough', () => {
-                                performRequentialRefresh();
-                            });
-                            mediaElement.addEventListener('error', () => {
-                                performRequentialRefresh();
-                            });
-                        }
-                    }
-                }
-
-                const playingClass = 'gleap-tour-video--playing';
-                const playPauseContainer = document.querySelector('.gleap-tour-video-playpause');
-
-                const videoElement = document.querySelector('.gleap-tour-video-obj');
-                if (videoElement) {
-                    const videoContainer = videoElement.closest('.gleap-tour-video');
-
-                    if (self.unmuted) {
-                        if (videoElement) {
-                            videoElement.pause();
-                            videoElement.muted = false;
-                            videoElement.play();
-                            videoContainer.classList.add(playingClass);
-                        }
-                    }
-
-                    videoElement.addEventListener('ended', function () {
-                        playPauseContainer.innerHTML = loadIcon("replay");
-                        playPauseContainer.classList.add("gleap-tour-video-svg--fullscreen")
-                        videoContainer.classList.remove(playingClass);
-                    });
-
-                    videoElement.addEventListener('play', function () {
-                        console.log("Video started");
-                        playPauseContainer.classList.remove("gleap-tour-video-svg--fullscreen")
-                    });
-
-                    if (playPauseContainer) {
-                        playPauseContainer.addEventListener('click', () => clickVideo());
-                    }
-
-                    const clickVideo = () => {
-                        if (videoElement.muted) {
-                            self.unmuted = true;
-
-                            videoElement.pause();
-                            videoElement.currentTime = 0;
-                            videoElement.muted = false;
-                            videoElement.play();
-
-                            playPauseContainer.innerHTML = loadIcon("mute");
-                            videoContainer.classList.add(playingClass);
-                        } else if (videoElement.paused) {
-                            videoElement.muted = false;
-                            videoElement.play();
-
-                            playPauseContainer.innerHTML = loadIcon("mute");
-                            videoContainer.classList.add(playingClass);
-                        } else {
-                            videoElement.pause();
-                            playPauseContainer.innerHTML = loadIcon("unmute");
-                            videoContainer.classList.remove(playingClass);
-
-                        }
-                    }
-                }
-            }
-        });
-        gleapTourObj.drive();
-
-        document.addEventListener("click", onDocumentClick);
+      var driverStep = {
+        disableActiveInteraction: disableInteraction,
+        mode: step.mode,
+        popover: {
+          description: message,
+          popoverClass: `gleap-tour-popover-${step.type} ${
+            !hasSender && "gleap-tour-popover-no-sender"
+          } ${config.allowClose && "gleap-tour-popover-can-close"}`,
+          ...(isClickMode
+            ? {
+                showButtons: [],
+              }
+            : {}),
+        },
+      };
+      if (step.selector && step.selector.length > 0) {
+        driverStep.element = step.selector;
+      }
+      driverSteps.push(driverStep);
     }
+
+    var buttons = ["next", "close"];
+
+    if (config.backButton) {
+      buttons.push("previous");
+    }
+
+    function onDocumentClick(evnt) {
+      try {
+        var gleapTourPopover = document.querySelector(".gleap-tour-popover");
+        if (gleapTourPopover && !gleapTourPopover.contains(evnt.target)) {
+          const stepIndex = gleapTourObj.getActiveIndex();
+          const step = steps[stepIndex];
+          const element = gleapTourObj.getActiveElement();
+
+          if (step?.mode === "CLICK" && evnt?.target !== element) {
+            const isInsideElement = element.contains(evnt?.target);
+
+            if (!isInsideElement) {
+              // Ignore clicks outside of the actual element.
+              return;
+            }
+          }
+
+          if (
+            (element && element.tagName === "INPUT") ||
+            step.mode === "INPUT" ||
+            evnt?.target?.id.includes("tooltip-svg")
+          ) {
+            // Prevent.
+          } else {
+            gleapTourObj.moveNext();
+          }
+        }
+      } catch (e) {}
+    }
+
+    const gleapTourObj = GleapTours({
+      showProgress: true,
+      steps: driverSteps,
+      showProgress: steps.length > 1,
+      allowClose: config.allowClose,
+      nextBtnText: config.nextText,
+      doneBtnText: config.doneText,
+      prevBtnText: config.prevText,
+      showButtons: buttons,
+      onDestroyStarted: () => {
+        if (!gleapTourObj.hasNextStep()) {
+          gleapTourObj.destroy();
+
+          if (self.onCompletion) {
+            self.onCompletion({
+              tourId: self.productTourId,
+            });
+          }
+        } else {
+          gleapTourObj.destroy();
+        }
+
+        document.removeEventListener("click", onDocumentClick);
+      },
+      onPopoverRender: (popoverElement) => {
+        // Fix for images and videos.
+        if (popoverElement) {
+          const mediaElements = document.querySelectorAll(
+            ".gleap-tour-popover-description img, .gleap-tour-popover-description video"
+          );
+
+          const performRequentialRefresh = () => {
+            setTimeout(() => {
+              gleapTourObj.refresh();
+            }, 500);
+            setTimeout(() => {
+              gleapTourObj.refresh();
+            }, 750);
+          };
+
+          for (let i = 0; i < mediaElements.length; i++) {
+            const mediaElement = mediaElements[i];
+            if (mediaElement.tagName === "IMG") {
+              mediaElement.addEventListener("load", () => {
+                performRequentialRefresh();
+              });
+              mediaElement.addEventListener("error", () => {
+                performRequentialRefresh();
+              });
+            } else if (mediaElement.tagName === "VIDEO") {
+              mediaElement.addEventListener("canplaythrough", () => {
+                performRequentialRefresh();
+              });
+              mediaElement.addEventListener("error", () => {
+                performRequentialRefresh();
+              });
+            }
+          }
+        }
+
+        const playingClass = "gleap-tour-video--playing";
+        const playPauseContainer = document.querySelector(
+          ".gleap-tour-video-playpause"
+        );
+
+        const videoElement = document.querySelector(".gleap-tour-video-obj");
+        if (videoElement) {
+          const videoContainer = videoElement.closest(".gleap-tour-video");
+
+          if (self.unmuted) {
+            if (videoElement) {
+              videoElement.pause();
+              videoElement.muted = false;
+              videoElement.play();
+              videoContainer.classList.add(playingClass);
+            }
+          }
+
+          videoElement.addEventListener("ended", function () {
+            playPauseContainer.innerHTML = loadIcon("replay");
+            playPauseContainer.classList.add(
+              "gleap-tour-video-svg--fullscreen"
+            );
+            videoContainer.classList.remove(playingClass);
+          });
+
+          videoElement.addEventListener("play", function () {
+            console.log("Video started");
+            playPauseContainer.classList.remove(
+              "gleap-tour-video-svg--fullscreen"
+            );
+          });
+
+          if (playPauseContainer) {
+            playPauseContainer.addEventListener("click", () => clickVideo());
+          }
+
+          const clickVideo = () => {
+            if (videoElement.muted) {
+              self.unmuted = true;
+
+              videoElement.pause();
+              videoElement.currentTime = 0;
+              videoElement.muted = false;
+              videoElement.play();
+
+              playPauseContainer.innerHTML = loadIcon("mute");
+              videoContainer.classList.add(playingClass);
+            } else if (videoElement.paused) {
+              videoElement.muted = false;
+              videoElement.play();
+
+              playPauseContainer.innerHTML = loadIcon("mute");
+              videoContainer.classList.add(playingClass);
+            } else {
+              videoElement.pause();
+              playPauseContainer.innerHTML = loadIcon("unmute");
+              videoContainer.classList.remove(playingClass);
+            }
+          };
+        }
+      },
+    });
+    gleapTourObj.drive();
+
+    document.addEventListener("click", onDocumentClick);
+  }
 }
