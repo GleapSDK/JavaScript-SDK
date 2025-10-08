518b74391d2679769a6b238eb191eee77af8459d
LucaB01 <luca@boehlerbrothers.com>
2025-10-08 09:02:06 +0200

    Fix URL regex to allow part matching

--- demo/main.js
+++ demo/main.js
@@ -1,13 +1,12 @@
 const Gleap = window.Gleap;
 
-// Gleap.setFrameUrl("http://0.0.0.0:3001");
-// Gleap.setApiUrl("http://0.0.0.0:9000");
-// Gleap.setWSApiUrl("ws://0.0.0.0:9000");
+Gleap.setFrameUrl("http://0.0.0.0:3001");
+Gleap.setApiUrl("http://0.0.0.0:9000");
+Gleap.setWSApiUrl("ws://0.0.0.0:9000");
 
-Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
+Gleap.initialize("H0fyJf8md5xrJn0W6ivHAlQeolIGFai1");
 
 Gleap.identify("38473294732892", {
   name: "Luca",
   email: "luca@gleap.io",
-  companyId: "hBB8dGXmRKZkaqKzptst",
 });
