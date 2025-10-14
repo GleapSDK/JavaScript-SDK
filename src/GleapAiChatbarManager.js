47bc1e20744348768f482a5e511f682faef6e316
Lukas Boehler <lukas@gleap.io>
2025-10-14 10:56:18 +0200

    v15.0.0

--- src/GleapAiChatbarManager.js
+++ src/GleapAiChatbarManager.js
@@ -271,10 +271,11 @@ export default class GleapAiChatbarManager {
         bottom: 20px;
         left: 0;
         right: 0;
+        height: 0;
         z-index: 999999;
         display: flex;
         flex-direction: column;
-        justify-content: center;
+        justify-content: flex-end;
         align-items: center;
         opacity: 0;
         transform: translateY(20px);
