c0966b3ad2ee4f301f07ae35442748b51a636627
LucaB01 <luca@boehlerbrothers.com>
2026-06-29 09:18:12 +0200

    reset main.js

--- demo/main.js
+++ demo/main.js
@@ -12,10 +12,3 @@ Gleap.initialize('GnhEkS8fdwxNVjyn3BnYwKzpCkiHgKWL');
 // setTimeout(() => {
 //   Gleap.startBot("6908b32c145ee7d9aa226782");
 // }, 1000);
-
-Gleap.identify('535506', {
-  email: 'luca@gleap.io',
-  // customData: {
-  //   user_token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1c2Vycy1zZXJ2aWNlIiwiaWF0IjoxNzgyNDc3OTc2LCJleHAiOjE3ODI1NjQzNzUsInVzZXJfaWQiOjE4MTEyNCwidG9rZW4iOiIwZlF4WXVzbnRNRTlvOVFSYllndkNhb0tWWmtwTXprbFl4VGo4NFYycElCZEJzcHFlSFV0WktvbVBQbk1mVmh3IiwidG9rZW5fZXhwaXJlZCI6IjIwMjYtMDYtMjdUMTI6NDY6MTYuMDAwMDAwWiIsIm1vbm9saXRoX3VzZXJfaWQiOjE4NzI0MSwib3JnYW5pemF0aW9uX2lkIjpudWxsLCJoYXNfcGFuZWxfYWNjZXNzIjpmYWxzZSwidjIiOnsidmVyaWZpY2F0aW9uVHlwZSI6InVzZXIiLCJ1c2VySWQiOiIxODExMjQiLCJhZmZpbGlhdGUiOiJnb3ZvbGEiLCJlbWFpbCI6ImRyYWdvcy5wYXNjYWxlQHZvbGEucm8iLCJyb2xlcyI6W119fQ.LWKVpZ6qzugJ6zi8Q7AgxUHATH3tL_-pq1s2TSr7b5c',
-  // },
-});
